288d33b378ebf33b9cbc9f1e1207d8e03ff662cc
tientran1234 <tranngoctien29112003@gmail.com>
2025-07-06 14:33:14 +0700

    return logic booking

--- apps/booking/src/booking.service.ts
+++ apps/booking/src/booking.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { BookingStatus } from '@prisma/client';
 import { InvalidCategoryIdException } from 'libs/common/src/errors/share-category.error';
 import { ServiceProviderNotFoundException } from 'libs/common/src/errors/share-provider.error';
 
 import { SharedProviderRepository } from 'libs/common/src/repositories/share-provider.repo';
+import { SharedBookingRepository } from 'libs/common/src/repositories/shared-booking.repo';
 import { SharedCategoryRepository } from 'libs/common/src/repositories/shared-category.repo';
 import { CreateServiceRequestBodyType } from 'libs/common/src/request-response-type/booking/booking.model';
 import { RabbitService } from 'libs/common/src/services/rabbit.service';
@@ -14,7 +16,7 @@ export class BookingsService {
     private readonly bookingRepository: BookingRepository,
     private readonly sharedProviderRepository: SharedProviderRepository,
     private readonly sharedCategoriesRepository: SharedCategoryRepository,
-
+    private readonly sharedBookingsRepository: SharedBookingRepository,
     private readonly rabbit: RabbitService,
 
   ) { }
@@ -24,16 +26,16 @@ export class BookingsService {
     const [category, provider] = await Promise.all([this.sharedCategoriesRepository.findUnique([body.categoryId]), this.sharedProviderRepository.findUnique({ id: body.providerId })])
     if (category.length < 1) throw InvalidCategoryIdException([body.categoryId])
     if (!provider) throw ServiceProviderNotFoundException
-    return await this.bookingRepository.createServiceRequest(body, customerId)
-    // return this.sharedBookingsRepository.create({
-    //   customerId: customerId,
-    //   providerId: body.providerId,
-    //   status: BookingStatus.PENDING,
-    //   serviceRequestId: serviceRequest.id,
+    const serviceRequest = await this.bookingRepository.createServiceRequest(body, customerId)
+    return this.sharedBookingsRepository.create({
+      customerId: customerId,
+      providerId: body.providerId,
+      status: BookingStatus.PENDING,
+      serviceRequestId: serviceRequest.id,
 
 
 
-    // })
+    })
   }
 
 }
